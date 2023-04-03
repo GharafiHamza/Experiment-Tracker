f0881996f9bf8a294aa4e49822250c072f49ac44
GharafiHamza <gharafi.hamza@gmail.com>
2023-04-03 06:05:38 +0000

    just trying

--- starting_code/extrace.py
+++ starting_code/extrace.py
@@ -2,9 +2,9 @@ import trace
 import numpy as np
 
 def extrace(fun):
-    def wrapper(*args):
+    def wrapper(*args, **kwargs):
         tracer = trace.Trace(trace=1, count=1)
-        tracer.runfunc(fun,*args)
+        tracer.runfunc(fun,*args, **kwargs)
         return tracer.results()
     return wrapper
 
@@ -17,5 +17,5 @@ if __name__ == "__main__":
         else:
             arr = [0]
         return np.sum(arr)
-
-    add(2,3).write_results(show_missing=True, summary=False, coverdir="coverage")
+    
+    print(add(2,3).write_results(show_missing=False, summary=True, coverdir="coverage"))
